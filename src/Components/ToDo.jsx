1d9937337537b604ca7aad180af50647a2858956
Abuzar-cpu <kerimovabuzer25@gmail.com>
2022-03-04 12:22:52 +0400

    delete button and change page buttons work now

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -1,4 +1,3 @@
-//@ts-check
 import React, {createRef} from "react";
 import ToDoItem from "./ToDoItem";
 import './CSS/ToDo.css'
@@ -12,35 +11,69 @@ export default class ToDo extends React.Component {
         this.state = {
             completed: [],
             pending: [],
-            deleted: []
+            deleted: [],
+            activePage: "pending"
         }
 
         this.inpRef = createRef();
         this.delete = this.delete.bind(this);
+        this.complete = this.complete.bind(this);
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("Component updated:", this.state);
+    complete(note) {
+        let newCompleted = [...this.state.completed, note];
+        let newPending = this.state.pending.filter((item) => item !== note);
+
+        this.setState({
+            completed: newCompleted,
+            pending: newPending,
+        });
     }
 
     delete(note) {
-        console.log(note);
-        // let newCompleted = [...this.state.completed];
-        // let newPending = [...this.state.pending];
-        // this.state.completed.indexOf(note) !== -1 ? newPending.push(note) : newCompleted.push(note);
-        //
-        // this.setState({
-        //    completed: newCompleted,
-        //    pending: newPending,
-        //    deleted: [...this.state.deleted, note]
-        // });
+        let newCompleted = [...this.state.completed];
+        let newPending = [...this.state.pending];
+        if(this.state.completed.indexOf(note) !== -1) {
+            newCompleted = newCompleted.filter((item) => item !== note);
+        }
+
+        if(this.state.pending.indexOf(note) !== -1) {
+            newPending = newPending.filter((item) => item !== note);
+        }
+
+        this.setState({
+           completed: newCompleted,
+           pending: newPending,
+           deleted: [...this.state.deleted, note]
+        });
+
+        // this.completedButtonRef = createRef();
+        // this.notCompletedButtonRef = createRef();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("Showing:", this.state.activePage)
+    }
+
+    showNotCompleted() {
+        this.setState({
+            activePage: "pending"
+        });
+
+        // console.log(this.notCompletedButtonRef.current);
+    }
+
+    showCompleted() {
+        this.setState({
+            activePage: "completed"
+        })
     }
 
     render() {
         const handleEnter = (e) => {
             if(e.key === "Enter") {
                 // eslint-disable-next-line no-unused-expressions
-                this.inpRef.current.value.trim() !== "" ? this.setState({completed: [...this.state.completed, this.inpRef.current.value.trim()]}) : null;
+                this.inpRef.current.value.trim() !== "" ? this.setState({pending: [...this.state.completed, this.inpRef.current.value.trim()]}) : null;
                 this.inpRef.current.value = "";
             }
         }
@@ -61,7 +94,21 @@ export default class ToDo extends React.Component {
 
                 <div className={"row"}>
                     <div className={"col"}>
-                        {this.state.completed.map((item, idx) => <ToDoItem delete={this.delete} key={`todo-item-${idx}`}>{item}</ToDoItem>)}
+                        {this.state.activePage === "pending" ? this.state.pending.map((item, idx) => <ToDoItem completed={false} complete={this.complete} delete={this.delete} key={`todo-item-${idx}`}>{item}</ToDoItem>) : this.state.completed.map((item, idx) => <ToDoItem completed={true} complete={this.complete} delete={this.delete} key={`todo-item-${idx}`}>{item}</ToDoItem>)}
+                    </div>
+                </div>
+
+                <div className={"row w-50 ml-auto mr-auto"}>
+                    <div className={"col d-flex flex-row-reverse p-0"}>
+                        <button  onClick={() => this.showCompleted()}>
+                            Completed
+                        </button>
+                    </div>
+
+                    <div className={"col d-flex p-0"}>
+                        <button onClick={() => this.showNotCompleted()}>
+                            Not Completed
+                        </button>
                     </div>
                 </div>
             </div>
--- src/Components/ToDoItem.jsx
+++ src/Components/ToDoItem.jsx
@@ -1,17 +1,13 @@
-import React, {createRef} from "react";
+import React from "react";
 import './CSS/ToDo.css'
 
 export default class todoItem extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div id={"to-do-item"} className={"mr-auto ml-auto w-50 p-3 d-flex"}>
-                    <input type={"checkbox"} />
+                {this.props.completed ? <input defaultChecked={true} onInput={() => this.props.complete(this.props.children)} type={"checkbox"} /> : <input onInput={() => this.props.complete(this.props.children)} type={"checkbox"} />}
                 <div className={"ml-3"}>{this.props.children}</div>
-                <span onClick={this.props.delete(this.props.children)} className={"ml-auto"} ><i className="fa fa-times" /></span>
+                <span onClick={() => this.props.delete(this.props.children)} className={"ml-auto"} ><i className="fa fa-times" /></span>
             </div>
         )
     }
